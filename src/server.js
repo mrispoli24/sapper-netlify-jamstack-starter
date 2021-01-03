329b7e71905ae38cf6b6cd75bfbad8fc745dae23
Ed Johnson-Williams <edjw@users.noreply.github.com>
2021-01-03 20:33:20 +0000

    fix bug in server.js around http-proxy-middleware
    proxyMiddleware in http-proxy-middleware is actually (or now) called createProxyMiddleware

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
-import proxyMiddleware from 'http-proxy-middleware';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
@@ -10,23 +10,23 @@ const dev = NODE_ENV === 'development';
 const server = polka();
 
 const devProxy = {
-  '/.netlify': {
-    target: 'http://localhost:9000',
-    pathRewrite: { '^/.netlify/functions': '' }
-  }
+	'/.netlify': {
+		target: 'http://localhost:9000',
+		pathRewrite: { '^/.netlify/functions': '' }
+	}
 };
 
 // Set up the proxy.
 if (dev && devProxy) {
-  Object.keys(devProxy).forEach(function(context) {
-    server.use(proxyMiddleware(context, devProxy[context]));
-  });
+	Object.keys(devProxy).forEach(function (context) {
+		server.use(createProxyMiddleware(context, devProxy[context]));
+	});
 }
 
 server // You can also use Express
 	.use(
 		compression({ threshold: 0 }),
-    sirv('static', { dev }),
+		sirv('static', { dev }),
 		sapper.middleware()
 	)
 	.listen(PORT, err => {
